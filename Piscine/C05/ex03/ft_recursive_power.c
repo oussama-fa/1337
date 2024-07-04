38a356dd10a3ee3860f64ddb703fa5cd685c8fd1
oussama-fa <caaf4040@gmail.com>
2024-07-04 17:43:37 +0000

    The Future Is Loading

--- Piscine/C05/ex03/ft_recursive_power.c
+++ Piscine/C05/ex03/ft_recursive_power.c
@@ -2,8 +2,6 @@ int ft_recursive_power(int nb, int power)
 {
     if (power == 0)
         return (1);
-    else if (power == 1)
-        return (nb);
     else if (power < 0)
         return (0);
     return (ft_recursive_power(nb, power - 1) * nb);
