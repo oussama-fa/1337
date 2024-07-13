e288d66bb6278f2289e23891b317bf5fc94d578e
oussama-fa <caaf4040@gmail.com>
2024-07-13 14:30:11 +0000

    The Future Is Loading

---
+++ Piscine/C06/ex00/ft_print_program_name.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+// int main(int    ac, char    **av)
+// {
+//     int i;
+
+//     i = 0;
+//     if (ac >= 1)
+//     {
+//         while (av[0][i])
+//         {
+//             write(1, &av[0][i], 1);
+//             i++;
+//         }
+//     }
+//     write(1, "\n", 1);
+// }
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	main(int ac, char *av[])
+{
+	if (ac >= 1)
+		ft_putstr(av[0]);
+	write(1, "\n", 1);
+}
---
+++ Piscine/C06/ex02/ft_rev_params.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void    ft_putstr(char  *str)
+{
+    while (*str)
+    {
+        write(1, str, 1);
+        str++;
+    }
+}
+
+int main(int    ac, char    **av)
+{
+    int i;
+
+    i = 1;
+    if (ac > 1)
+    {
+        while (ac > i)
+        {
+            ft_putstr(av[ac - i]);
+            i++;
+            write(1, "\n", 1);
+        }
+    }
+}
